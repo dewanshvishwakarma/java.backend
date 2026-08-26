2e9ae5b5f2ec0f9dd43a962c0a1294ad173b2f38
dewansh vishwakarma <dewanshvishwakarma073@gmail.com>
2026-08-27 00:26:13 +0530

    java application using spring (form binding)
    make from using spring mvc

---
+++ form_binding-2/src/main/java/com/example/demo/FormBinding2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBinding2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormBinding2Application.class, args);
+		
+		
+	}
+
+}
